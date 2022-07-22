d5fa9eb1b2d5e104abeefcf777742c8a7ac30f18
DMG0411 <93600007+DMG0411@users.noreply.github.com>
2022-07-22 16:25:25 +0300

    Update SQLConnection.java

--- src/SQLConnection.java
+++ src/SQLConnection.java
@@ -9,7 +9,7 @@ public class SQLConnection {
     SQLConnection() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            this.conn = DriverManager.getConnection("jdbc:mysql:///carstocks", "root", "Ampulamare22");
+            this.conn = DriverManager.getConnection("jdbc:mysql:///carstocks", "root", "password");
             this.st = this.conn.createStatement(1004, 1008);
         } catch (Exception var2) {
             var2.printStackTrace();
